685ef0fae823dd1d16abdbc1999e085810b228b9
Matt Matteson <matt@mattmatteson.com>
2013-04-04 01:02:18 -0700

    Update balance for identity from each address. Update balance for each address from blockchain API.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,31 @@
 class Address < ActiveRecord::Base
   belongs_to :identity
+  attr_accessible :balance
+  
+  def update_balance
+    value = Address.value_for_key(address)
+    if value
+      update_attributes(:balance => value)
+    end
+  end
+  
+  def self.value_for_key(address)
+    url = "http://blockchain.info/address/#{address}?format=json"
+    uri = URI(url)
+    client = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = client.request(request)
+  
+    value = nil
+    if response.is_a?(Net::HTTPSuccess)
+      parsed_response = JSON.parse(response.body)
+      value = parsed_response["final_balance"]
+    else 
+      puts "Error accessing blockchain API"
+    end
+  
+    puts value
+    value
+  end
+  
 end
--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,8 +1,25 @@
 class Identity < ActiveRecord::Base
   validates_presence_of :name
-  attr_accessible :name
+  attr_accessible :name, :balance
+  has_many :addresses
 
   scope :leaderboard, -> {
       where('balance IS NOT NULL').order('balance DESC')
   }
+  
+  def update_balance
+    total = 0
+    self.addresses.each do |address|
+      address.update_balance
+      total = total + address.balance
+    end
+    update_attributes(:balance => total)
+  end
+  
+  def balance_in_bitcoin
+    return nil unless balance
+    btc = 0.00000001 * balance
+    
+    btc
+  end
 end
