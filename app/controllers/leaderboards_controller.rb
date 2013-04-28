f40e36b0ee86cb9834fa06d71d80b5a3b96dd5db
Matt Matteson <matt@mattmatteson.com>
2013-04-27 19:31:08 -0700

    Make sure there is a default selected identity.

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -1,12 +1,15 @@
 class LeaderboardsController < ApplicationController
   def show
-    # Worker.shared_instance
-    if params[:name]
-      @identity = Identity.where(['LOWER(name) = ?', params[:name].downcase]).first!  
-    end
-    
     #  find the identity in the whole set
     # grab a subset, say 5 before and 24 after
     @identities = Identity.leaderboard.limit(10)
+    
+    
+    # Worker.shared_instance
+    if params[:name]
+      @identity = Identity.where(['LOWER(name) = ?', params[:name].downcase]).first!  
+    else
+      @identity = @identities.first
+    end
   end
 end
